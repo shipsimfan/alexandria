880102bf85c3df14fb35bd4e46f03f8837aa1c36
Lance Hart <shipsimfan@hotmail.com>
2022-02-09 18:03:06 -0500

    Added mesh creation error to public

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,6 @@ mod window;
 
 pub use crate::graphics::{GraphicsCreationError, RenderError};
 pub use math::*;
-pub use mesh::Mesh;
+pub use mesh::{Mesh, MeshCreationError};
 pub use shader::{Format, Shader, ShaderCreationError};
 pub use window::{Window, WindowCreationError};
