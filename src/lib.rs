8893fc4d742add468e44d193bb13923bc0451e73
Lance Hart <shipsimfan@hotmail.com>
2022-04-04 21:15:06 -0400

    Added set constant buffer error to public

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ mod window;
 pub use crate::graphics::{GraphicsCreationError, RenderError};
 pub use math::*;
 pub use mesh::{Mesh, MeshCreationError};
-pub use shader::{Format, Shader, ShaderCB, ShaderCreationError};
+pub use shader::{Format, SetConstantBufferError, Shader, ShaderCB, ShaderCreationError};
 pub use window::{Window, WindowCreationError};
 
 pub use win32::ID3D11Device as Device;
