0052a6c997c5fa9e76d3ba89eb75aa5e08fee131
Lance Hart <shipsimfan@hotmail.com>
2022-05-24 20:23:56 -0400

    Fixed integration test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,8 +2,8 @@ use std::f32::consts::PI;
 
 #[repr(C)]
 struct Vertex {
-    position: graphics::Vector3,
-    color: graphics::Vector4,
+    position: alexandria::Vector3,
+    color: alexandria::Vector4,
 }
 
 #[repr(C)]
@@ -16,36 +16,36 @@ struct MatrixBuffer {
 
 const VERTICES: [Vertex; 8] = [
     Vertex {
-        position: graphics::Vector3::new(1.0, -1.0, 1.0),
-        color: graphics::Vector4::new(1.0, 0.0, 0.0, 1.0),
+        position: alexandria::Vector3::new(1.0, -1.0, 1.0),
+        color: alexandria::Vector4::new(1.0, 0.0, 0.0, 1.0),
     },
     Vertex {
-        position: graphics::Vector3::new(1.0, -1.0, -1.0),
-        color: graphics::Vector4::new(0.0, 1.0, 0.0, 1.0),
+        position: alexandria::Vector3::new(1.0, -1.0, -1.0),
+        color: alexandria::Vector4::new(0.0, 1.0, 0.0, 1.0),
     },
     Vertex {
-        position: graphics::Vector3::new(1.0, 1.0, -1.0),
-        color: graphics::Vector4::new(0.0, 0.0, 1.0, 1.0),
+        position: alexandria::Vector3::new(1.0, 1.0, -1.0),
+        color: alexandria::Vector4::new(0.0, 0.0, 1.0, 1.0),
     },
     Vertex {
-        position: graphics::Vector3::new(1.0, 1.0, 1.0),
-        color: graphics::Vector4::new(1.0, 1.0, 0.0, 1.0),
+        position: alexandria::Vector3::new(1.0, 1.0, 1.0),
+        color: alexandria::Vector4::new(1.0, 1.0, 0.0, 1.0),
     },
     Vertex {
-        position: graphics::Vector3::new(-1.0, -1.0, 1.0),
-        color: graphics::Vector4::new(1.0, 0.0, 1.0, 1.0),
+        position: alexandria::Vector3::new(-1.0, -1.0, 1.0),
+        color: alexandria::Vector4::new(1.0, 0.0, 1.0, 1.0),
     },
     Vertex {
-        position: graphics::Vector3::new(-1.0, -1.0, -1.0),
-        color: graphics::Vector4::new(0.0, 1.0, 1.0, 1.0),
+        position: alexandria::Vector3::new(-1.0, -1.0, -1.0),
+        color: alexandria::Vector4::new(0.0, 1.0, 1.0, 1.0),
     },
     Vertex {
-        position: graphics::Vector3::new(-1.0, 1.0, -1.0),
-        color: graphics::Vector4::new(1.0, 1.0, 1.0, 1.0),
+        position: alexandria::Vector3::new(-1.0, 1.0, -1.0),
+        color: alexandria::Vector4::new(1.0, 1.0, 1.0, 1.0),
     },
     Vertex {
-        position: graphics::Vector3::new(-1.0, 1.0, 1.0),
-        color: graphics::Vector4::new(0.0, 0.0, 0.0, 1.0),
+        position: alexandria::Vector3::new(-1.0, 1.0, 1.0),
+        color: alexandria::Vector4::new(0.0, 0.0, 0.0, 1.0),
     },
 ];
 const INDICES: [u32; 36] = [
@@ -55,28 +55,29 @@ const INDICES: [u32; 36] = [
 
 #[test]
 fn test() {
-    let mut window: Box<graphics::Window> = graphics::Window::new("Testing", 1920, 1080).unwrap();
+    let mut window: Box<alexandria::Window> =
+        alexandria::Window::new("Testing", 1920, 1080).unwrap();
 
     let shader_code = std::fs::read_to_string("./default.hlsl").unwrap();
-    let translation = graphics::Matrix::translation(0.0, 0.0, 3.0);
+    let translation = alexandria::Matrix::translation(0.0, 0.0, 3.0);
     let mut matrix_buffer = MatrixBuffer {
         world: translation.into(),
-        view: graphics::Matrix::identity().into(),
-        projection: graphics::Matrix::perspective(PI / 2.0, 16.0 / 9.0, 0.01, 100.0).into(),
+        view: alexandria::Matrix::identity().into(),
+        projection: alexandria::Matrix::perspective(PI / 2.0, 16.0 / 9.0, 0.01, 100.0).into(),
     };
-    let mut shader = graphics::Shader::new(
+    let mut shader = alexandria::Shader::new(
         shader_code,
         &[
-            ("POSITION", graphics::Format::R32G32A32Float),
-            ("COLOR", graphics::Format::R32G32B32A32Float),
+            ("POSITION", alexandria::Format::R32G32A32Float),
+            ("COLOR", alexandria::Format::R32G32B32A32Float),
         ],
         &mut window,
     )
     .unwrap();
     let mut constant_buffer =
-        graphics::ConstantBuffer::<_, 0>::new(Some(matrix_buffer), &mut window).unwrap();
+        alexandria::ConstantBuffer::<_, 0>::new(Some(matrix_buffer), &mut window).unwrap();
 
-    let mut mesh = graphics::Mesh::new(&VERTICES, &INDICES, &mut window).unwrap();
+    let mut mesh = alexandria::Mesh::new(&VERTICES, &INDICES, &mut window).unwrap();
 
     let mut y_rotation = 0.0;
     let mut x_rotation = 0.0;
@@ -123,8 +124,8 @@ fn test() {
         }
 
         matrix_buffer.world = (translation
-            * graphics::Matrix::rotation_y(y_rotation)
-            * graphics::Matrix::rotation_x(x_rotation))
+            * alexandria::Matrix::rotation_y(y_rotation)
+            * alexandria::Matrix::rotation_x(x_rotation))
         .into();
     }
 }
