453df62124c9f6fe546efc419ab01bf5e0f5bbac
Lance Hart <shipsimfan@hotmail.com>
2023-05-21 21:13:30 -0400

    Added validation layers

--- src/instance.rs
+++ src/instance.rs
@@ -7,6 +7,29 @@ pub struct Instance {
     os_instance: os::Instance,
 }
 
+const VALIDATION_LAYER_NAME: &str = "VK_LAYER_KHRONOS_validation";
+const VALIDATION_LAYER_NAME_TERMINATED: &str = "VK_LAYER_KHRONOS_validation\0";
+
+fn collect_extensions() -> Vec<*const u8> {
+    os::get_extension_list()
+}
+
+fn collect_layers(vulkan: &Vulkan) -> Result<Vec<*const u8>> {
+    #[cfg(debug_assertions)]
+    {
+        for layer in vulkan
+            .enumerate_instance_layer_properties()
+            .map_err(|error| create_error!(VulkanInstanceCreationFailed, Some(Vulkan(error))))?
+        {
+            if layer.layer_name() == VALIDATION_LAYER_NAME {
+                return Ok(vec![VALIDATION_LAYER_NAME_TERMINATED.as_ptr()]);
+            }
+        }
+    }
+
+    Ok(Vec::new())
+}
+
 impl Instance {
     pub fn new(app_name: &str, app_version: u32) -> Result<Arc<Self>> {
         assert_eq!(app_name.as_bytes()[app_name.len() - 1], 0);
@@ -27,8 +50,8 @@ impl Instance {
                     0,
                     VK_API_VERSION_1_0,
                 )),
-                &[],
-                os::get_extension_list(),
+                &collect_layers(&vulkan)?,
+                &collect_extensions(),
             ))
             .map_err(|error| create_error!(VulkanInstanceCreationFailed, Some(Vulkan(error))))?;
 
--- windows/src/extensions.rs
+++ windows/src/extensions.rs
@@ -3,6 +3,6 @@ const WIN32_SURFACE_EXTENSION: &str = "VK_KHR_win32_surface\0";
 
 const EXTENSIONS: &[*const u8] = &[SURFACE_EXTENSION.as_ptr(), WIN32_SURFACE_EXTENSION.as_ptr()];
 
-pub fn get_extension_list() -> &'static [*const u8] {
-    EXTENSIONS
+pub fn get_extension_list() -> Vec<*const u8> {
+    Vec::from(EXTENSIONS)
 }
