d8233b67fc2a8e07d879c56e408cd56b0eaa1202
Lance Hart <me@lancehart.ca>
2024-07-31 21:37:45 -0400

    Fixed time zone to adapt for daylight savings

--- vk/src/windows/time_zone.rs
+++ vk/src/windows/time_zone.rs
@@ -1,11 +1,19 @@
-use win32::{GetTimeZoneInformation, TIME_ZONE_ID_INVALID, TIME_ZONE_INFORMATION};
+use win32::{
+    GetTimeZoneInformation, TIME_ZONE_ID_DAYLIGHT, TIME_ZONE_ID_INVALID, TIME_ZONE_INFORMATION,
+};
 
 /// Gets the current system time zone, represented as minutes away from UTC
 pub fn get_time_zone() -> i16 {
     let mut time_zone_info = TIME_ZONE_INFORMATION::default();
-    if (unsafe { GetTimeZoneInformation(&mut time_zone_info) } == TIME_ZONE_ID_INVALID) {
+    let result = unsafe { GetTimeZoneInformation(&mut time_zone_info) };
+    if result == TIME_ZONE_ID_INVALID {
         return 0;
     }
 
-    -time_zone_info.bias as i16
+    -(time_zone_info.bias
+        + if result == TIME_ZONE_ID_DAYLIGHT {
+            time_zone_info.daylight_bias
+        } else {
+            time_zone_info.standard_bias
+        }) as i16
 }
