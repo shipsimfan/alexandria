a1b2ee5603468e6e588c5241af1f7e2afe39c8af
Lance Hart <me@lancehart.ca>
2025-01-26 19:02:35 -0500

    Added enum_adapters call

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,8 @@
 use std::ptr::null_mut;
 use win32::{
     dxgi::{CreateDXGIFactory, IDXGIFactory},
-    try_hresult, COMInterface, COMPtr,
+    strsafe::S_OK,
+    try_hresult, COMInterface, COMPtr, DXGI_ERROR_NOT_FOUND,
 };
 
 /// Enumerates all the available adapters on the system
@@ -17,9 +18,22 @@ pub fn enumerate_adapters() -> Result<(), win32::Error> {
     // Create the factory
     let mut factory = null_mut();
     try_hresult!(CreateDXGIFactory(&IDXGIFactory::IID, &mut factory)).unwrap();
-    let factory = COMPtr::new(factory as *mut IDXGIFactory);
+    let mut factory = COMPtr::new(factory as *mut IDXGIFactory);
 
-    drop(factory);
+    let mut i = 0;
+    loop {
+        let mut adapter = null_mut();
+        let result = factory.enum_adapters(i, &mut adapter);
+        if result == S_OK {
+            println!("Adapter found!");
+        } else if result == DXGI_ERROR_NOT_FOUND {
+            break;
+        } else {
+            panic!("Invalid result: {}", result);
+        }
+
+        i += 1;
+    }
 
     Ok(())
 }
