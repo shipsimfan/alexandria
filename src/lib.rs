2f61de210da0aa7570c4390ae10edf9952996a51
Lance Hart <me@lancehart.ca>
2026-01-31 14:21:51 -0500

    Dropping workspace

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,3 @@
 #![deny(rustdoc::unescaped_backticks)]
 #![deny(rustdoc::redundant_explicit_links)]
 #![warn(rustdoc::broken_intra_doc_links)]
-
-pub use alexandria_graphics as graphics;
-pub use alexandria_math as math;
-pub use alexandria_system as system;
-pub use alexandria_util as util;
