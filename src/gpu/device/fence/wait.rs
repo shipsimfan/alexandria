bf9101ff4d5145214de918a34bbdb88e3b7bebad
Lance Hart <me@lancehart.ca>
2026-09-13 18:52:27 -0400

    Update fence wait

--- src/gpu/device/fence/wait.rs
+++ src/gpu/device/fence/wait.rs
@@ -1,18 +1,19 @@
-use crate::{Error, Result, gpu::VulkanFence};
-use vulkan::try_vulkan;
+use crate::{
+    Error, Result,
+    gpu::{VulkanFence, VulkanResult},
+};
+use vulkan::{VK_TRUE, try_vulkan};
 
 impl VulkanFence {
     /// Waits for the fence to be signaled
-    pub fn wait(&self, timeout: u64) -> Result<()> {
+    pub fn wait(&self, timeout: u64) -> Result<VulkanResult> {
         try_vulkan!((self.device.functions().fence.wait_for_fences)(
             self.device.handle(),
             1,
             &self.handle,
-            0,
+            VK_TRUE,
             timeout,
         ))
-        .map_err(|vk| Error::new_with("unable to wait on a fence", vk))?;
-
-        Ok(())
+        .map_err(|vk| Error::new_with("unable to wait on a fence", vk))
     }
 }
